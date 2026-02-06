0e138e04e2f77a61a6fc6997e74d444353bc261e
AKR9634 <akhil6july2003@gmail.com>
2026-02-07 00:12:01 +0530

    Fast API and Model Evaluation and pusher added!!!

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -20,8 +20,8 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
-AWS_ACCESS_KEY_ID_ENV_KEY = ""
-AWS_SECRET_ACCESS_KEY_ENV_KEY = ""
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
 REGION_NAME = "us-east-1"
 
 
@@ -59,7 +59,7 @@ MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
 
 """ Model Evaluation related constants """
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "my-model-mlopsproj"
+MODEL_BUCKET_NAME = "akr-mlopsproj"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
